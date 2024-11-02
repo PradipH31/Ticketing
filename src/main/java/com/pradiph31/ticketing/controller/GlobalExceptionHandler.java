e7bd4b4ab57ae6ff28dd59822daf9ca5cdf90336
PradipH31 <pradip.hamal31@gmail.com>
2024-11-02 18:24:49 -0500

    Handled exceptions with API response

---
+++ src/main/java/com/pradiph31/ticketing/controller/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.pradiph31.ticketing.controller;
+
+import com.pradiph31.ticketing.dto.ErrorResponseDTO;
+import com.pradiph31.ticketing.exception.InvalidEventDataException;
+import com.pradiph31.ticketing.exception.InvalidTicketDataException;
+import com.pradiph31.ticketing.exception.NonexistentEventIDException;
+import com.pradiph31.ticketing.exception.NonexistentTicketIDException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+  @ExceptionHandler({NonexistentTicketIDException.class, NonexistentEventIDException.class})
+  public ResponseEntity<ErrorResponseDTO> handleNonexistentTicketIDException(RuntimeException exception) {
+    return ResponseEntity.status(404).body(new ErrorResponseDTO(exception.getMessage(), LocalDateTime.now(),
+            HttpStatus.NOT_FOUND.value()));
+  }
+
+  @ExceptionHandler({InvalidTicketDataException.class, InvalidEventDataException.class})
+  public ResponseEntity<ErrorResponseDTO> handleInvalidDataException(RuntimeException exception) {
+    return ResponseEntity.status(400).body(new ErrorResponseDTO(exception.getMessage(), LocalDateTime.now(),
+            HttpStatus.BAD_REQUEST.value()));
+  }
+}
