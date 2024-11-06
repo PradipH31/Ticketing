c8babe60452adc4e48cde3534f7af51e5976a0bc
PradipH31 <pradip.hamal31@gmail.com>
2024-11-06 09:43:33 -0600

    Added test to ensure context is loaded

--- src/test/java/com/pradiph31/ticketing/TicketingApplicationTests.java
+++ src/test/java/com/pradiph31/ticketing/TicketingApplicationTests.java
@@ -1,13 +1,26 @@
 package com.pradiph31.ticketing;
 
+import com.pradiph31.ticketing.repository.EventRestRepository;
+import com.pradiph31.ticketing.repository.TicketRestRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class TicketingApplicationTests {
 
+	@Autowired
+	private EventRestRepository eventRestRepository;
+
+	@Autowired
+	private TicketRestRepository ticketRestRepository;
+
 	@Test
 	void contextLoads() {
+		assertThat(eventRestRepository).isNotNull();
+		assertThat(ticketRestRepository).isNotNull();
 	}
 
 }
